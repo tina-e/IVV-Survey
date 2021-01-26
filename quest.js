400f241946d392ab3d26a821c509c1f47bdee6cd
Max Huber <Maximilian2.Huber@stud.uni-regensburg.de>
2021-01-26 18:21:38 +0100

    erstes aufsetzten
    erstmal js und html datei und erste frage gemacht

---
+++ quest.js
@@ -0,0 +1,17 @@
+function init(){
+    document.querySelector('.submit').addEventListener("click", endQuestionary);
+}
+
+function endQuestionary(){
+    console.log("AMERIKA STINKT");
+}
+
+
+
+
+
+
+
+
+
+init();
