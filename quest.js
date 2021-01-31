9d2e88ecbf7092cca5fdd12557f52eb43b545a2a
Max Huber <Maximilian2.Huber@stud.uni-regensburg.de>
2021-01-31 02:22:40 +0100

    mehr fragen rein
    weitere fragen

--- quest.js
+++ quest.js
@@ -7,13 +7,26 @@ function endQuestionary(){
     let gender;
     for (let i = 0; i < 3; i++) {
         if(document.getElementById("gender").children[i].children[0].checked){
-            gender = i;
+            gender = i+1;
         }  
     }
 
     let age = document.getElementById("age").value;
 
-    let jsonText = '{ "gender":"' + gender + '", "age":"' + age + '"  }';
+    let profession = document.getElementById("prof").value;
+
+    let langLevel;
+    for (let i = 0; i < 5; i++) {
+        if(document.getElementById("language").children[i].children[0].checked){
+            langLevel = i+1;
+        }  
+    }
+
+    let matNummer = document.getElementById("matNummer").value;
+    
+    let email = document.getElementById("mail").value; 
+
+    let jsonText = '{ "gender":"' + gender + '", "age":"' + age + '",  "profession":"' + profession + '","langLevel":"' + langLevel + '","matNummer":"' + matNummer + '","email":"' + email + '"}';
     let jsonObjekt = JSON.parse(jsonText);
     addToServer(jsonObjekt);
 }
@@ -21,13 +34,5 @@ function endQuestionary(){
 function addToServer(jsonObjekt){
     console.log(jsonObjekt);
 }
-
-
-
-
-
-
-
-
 
 init();
